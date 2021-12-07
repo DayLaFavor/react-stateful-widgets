bc15178486130127aa6079c00e86574239306703
DayLaFavor <danelafavor1@gmail.com>
2021-12-06 21:08:16 -0600

    Done

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -1,17 +1,14 @@
 /*
 PROGRAMMERS Instructions
-
 Watch this short video:
 https://tk-assets.lambdaschool.com/1ea6e6a2-2ef1-45de-bcab-b99a8f775870_programmers.gif
-
 This component keeps track of a list of pioneers in the field of programming on the one hand,
 and the id of the currently featured programmer on the other. That's two slices of state!
 We can only feature one awesome programmer at a time.
-
 Find comments below to help you along.
 */
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 // Use this variable ONLY to initialize a slice of state!
 // There is something in the JSX right now breaking this rule...
@@ -27,8 +24,8 @@ export const listOfAwesome = [
 export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
-const [Programmers] = useState(listOfAwesome)
-const  [list, setList] = useState(null)
+const [programmers]= useState(listOfAwesome)
+const [featured, setFeatured] = useState(null)
 
   const getNameOfFeatured = () => {
     // Leave this for last!
@@ -36,13 +33,13 @@ const  [list, setList] = useState(null)
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-    return Programmers.find(prog.id === list).name
+    return programmers[featured].name
   };
 
   const style = {
     fontSize: '1.5em',
     marginTop: '0.5em',
-    color: list ? 'gold' : 'royalblue'
+    color: featured ? 'gold' : 'royalblue', // 🤔 color turns to gold, when celebrating
   };
 
   return (
@@ -53,9 +50,9 @@ const  [list, setList] = useState(null)
           /* Nasty bug! We should map over a slice of state, instead of 'listOfAwesome'.
           We might think: "it works, though!" But if the list of programmers is not state,
           we could never add or edit programmers in the future. The list would be a static thing." */
-          listOfAwesome.map(dev =>
+          programmers.map(dev =>
             <div className='programmer' key={dev.id}>
-              {dev.name} <button onClick={() => {setList(dev.id)}}>Feature</button>
+              {dev.name} <button onClick={() => { setFeatured(dev.id - 1) /* in here set the featured id to be dev.id */ }}>Feature</button>
             </div>
           )
         }
@@ -65,9 +62,7 @@ const  [list, setList] = useState(null)
           // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
           // Pseudo-code: if the currently featured id is truthy render text 1, otherwise render text 2.
           // Replace the hard-coded false with the correct variable.
-          false
-            ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳`
-            : 'Pick an awesome programmer'
+          featured ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳` : 'Pick an awesome programmer'
         }
       </div>
     </div>
